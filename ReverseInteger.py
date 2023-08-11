d7a2fcc9cd33def8235a8ea1e92a2c5f43e73597
Amha Mersha <69669816+amha-mersha@users.noreply.github.com>
2023-08-11 10:04:48 +0300

    ReverseInteger.py

--- ReverseInteger.py
+++ ReverseInteger.py
@@ -11,3 +11,19 @@ class Solution(object):
         if result > 2 ** 31 - 1 or result < -(2 ** 31):
             return 0
         return result
+# not accepted by leetcode but worked on local machine 
+"""import math
+class Solution(object):
+    def reverse(self, x):
+        max = 2147483647
+        min = -214483468
+        result = 0
+        while x:
+            temp = int(math.fmod(x,10))
+            x = int(x/10)
+            if (result > (max//10) or (result == (max//10) and temp >= (max%10))):
+                return 0
+            if (result < (min//10) or (result == (min//10) and temp <= (min%10))):
+                return 0
+            result = (result*10) + temp
+        return result """
