97acb3320b74d38943d058ebe818057b9b3359a5
Amha Mersha <69669816+amha-mersha@users.noreply.github.com>
2023-08-26 20:26:16 +0300

    Update FindTargetIndicesAfterSortingArray.py

--- FindTargetIndicesAfterSortingArray.py
+++ FindTargetIndicesAfterSortingArray.py
@@ -6,3 +6,14 @@ class Solution(object):
             if i == target:
                 num.append(j)
         return num
+#another solution 
+class Solution(object):
+    def targetIndices(self, nums, target):
+        count = 0
+        dups = 0
+        for i in nums:
+            if i == target:
+                dups += 1  
+            elif i < target:
+                count += 1
+        return list(range(count,count+dups))
