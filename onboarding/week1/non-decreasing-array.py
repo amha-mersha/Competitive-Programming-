6e4b2dbefdd0f44e3ba6d00957e5a2ddcd287b10
Amha Mersha <69669816+amha-mersha@users.noreply.github.com>
2023-12-07 08:58:41 +0300

    Added solution for Non-decreasing Array

---
+++ onboarding/week1/non-decreasing-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def checkPossibility(self, nums):
+        changed = False
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                if changed:
+                    return False
+                if i == 0 or nums[i+1] >= nums[i-1]:
+                    nums[i] = nums[i+1]
+                else:
+                    nums[i+1] = nums[i]
+                changed = True
+        return True 
