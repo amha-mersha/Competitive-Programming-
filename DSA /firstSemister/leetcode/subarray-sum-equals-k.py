9e8cc74d882cbea189fb13d28f0fa31f221679fb
Amha Mersha <69669816+amha-mersha@users.noreply.github.com>
2024-02-08 10:08:32 +0300

    Add solution for Subarray Sum Equals K

--- DSA /firstSemister/leetcode/subarray-sum-equals-k.py
+++ DSA /firstSemister/leetcode/subarray-sum-equals-k.py
@@ -2,12 +2,12 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefix = defaultdict(int)
         prefix[0] += 1
-        running = 0
+        runSum = 0
         count = 0
         for i in nums:
-            running += i
-            if running - k in prefix:
-                count += prefix[running-k]
-            prefix[running] += 1
+            runSum += i
+            if runSum - k in prefix:
+                count += prefix[runSum-k]
+            prefix[runSum] += 1
         return count
         
