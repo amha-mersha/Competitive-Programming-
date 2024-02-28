d16b9c4535fac2d2bd6dfadcf8ab02c224f3fe47
Amha Mersha <69669816+amha-mersha@users.noreply.github.com>
2024-02-28 11:27:16 +0300

    Add solution for Count Good Numbers

--- DSA/firstSemister/camp/leetcode/count-good-numbers.py
+++ DSA/firstSemister/camp/leetcode/count-good-numbers.py
@@ -1,3 +1,14 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        return pow(20,n//2,10**9+7) if n%2 == 0 else (pow(20,n//2,10**9+7)*5)%(10**9+7) 
+        if n == 1:
+            return 5
+        def helper(n):
+            if n == 2:
+                return 20
+            if n%4 == 0:
+                curr = (helper(n//2))%(10**9 + 7)
+                return (curr*curr)%(10**9 + 7)
+            if n%2 == 1:
+                return (5* helper(n-1))%(10**9 + 7)
+            return (4* helper(n-1))%(10**9 + 7)
+        return helper(n)
